7757c7e3c8231fa92b9168f356687c2815791ea3
pi <pi@pi>
2019-07-27 07:29:41 -0300

    Welcome and menu GUI

--- AutoMatric/src/AutoMatric.java
+++ AutoMatric/src/AutoMatric.java
@@ -1,8 +1,83 @@
+import java.awt.EventQueue;
+
+import javax.imageio.ImageIO;
+import javax.print.DocFlavor.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Canvas;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.ActionEvent;
 
 public class AutoMatric {
 
+    private JFrame frmAutomatric;
+
+    /**
+     * Launch the application.
+     */
     public static void main(String[] args) {
-        
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    AutoMatric window = new AutoMatric();
+                    window.frmAutomatric.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
+    /**
+     * Create the application.
+     */
+    public AutoMatric() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frmAutomatric = new JFrame();
+        frmAutomatric.getContentPane().setBackground(Color.GRAY);
+        frmAutomatric.getContentPane().setLayout(null);
+        
+        JLabel lblNewLabel = new JLabel("");
+        Image logo = new ImageIcon(this.getClass().getResource("/lg_matric.png")).getImage();
+        lblNewLabel.setBounds(50, 40, 400, 103);
+        lblNewLabel.setIcon(new ImageIcon(logo));
+        frmAutomatric.getContentPane().add(lblNewLabel);
+        
+        JButton btnIni = new JButton("Iniciar !");
+        btnIni.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                frmAutomatric.dispose();
+                menu menu = new menu();
+                menu.setVisible(true);
+            }
+        });
+        btnIni.setBounds(200, 220, 100, 30);
+        frmAutomatric.getContentPane().add(btnIni);
+        frmAutomatric.setTitle("AutoMatric");
+        frmAutomatric.setResizable(false);
+        frmAutomatric.setBackground(Color.DARK_GRAY);
+        frmAutomatric.setBounds(100, 100, 500, 350);
+        frmAutomatric.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        try {
+        frmAutomatric.setIconImage(ImageIO.read(new File("img/icon.png")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        frmAutomatric.setLocationRelativeTo(null);
+    }
 }
---
+++ AutoMatric/src/menu.java
@@ -0,0 +1,69 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import java.awt.Image;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.ActionEvent;
+
+public class menu extends JFrame {
+
+    private JPanel contentPane;
+
+    public menu() {
+        setBackground(Color.DARK_GRAY);
+        setTitle("AutoMatric");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setBounds(100, 100, 500, 350);
+        contentPane = new JPanel();
+        contentPane.setBackground(Color.LIGHT_GRAY);
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        
+        JLabel lblNewLabel = new JLabel("");
+        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        Image logo2 = new ImageIcon(this.getClass().getResource("/lg.png")).getImage();
+        lblNewLabel.setBounds(40, 68, 175, 175);
+        lblNewLabel.setIcon(new ImageIcon(logo2));
+        contentPane.add(lblNewLabel);
+        
+        JButton btnClassificar = new JButton("Selecionar Arquivos");
+        btnClassificar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+            }
+        });
+        btnClassificar.setBounds(280, 117, 153, 26);
+        contentPane.add(btnClassificar);
+        
+        JButton button = new JButton("Classificar");
+        button.setBounds(280, 67, 153, 26);
+        contentPane.add(button);
+        
+        JButton button_1 = new JButton("Adicionar Aluno");
+        button_1.setBounds(280, 167, 153, 26);
+        contentPane.add(button_1);
+        
+        JButton button_2 = new JButton("Adicionar Matéria");
+        button_2.setBounds(280, 217, 153, 26);
+        contentPane.add(button_2);
+        
+        try {
+            super.setIconImage(ImageIO.read(new File("img/icon.png")));
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
+        setLocationRelativeTo(null);
+    }
+}
