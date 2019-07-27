c930482dc6d4d6b7c936a81712cb4ff2b503d254
miriboshi <m.iriboshiii@gmail.com>
2019-07-27 17:21:56 -0300

    Criação classes. A definir: metodos.

---
+++ AutoMatric/src/Bacharelado.java
@@ -0,0 +1,26 @@
+
+public class Bacharelado {
+	private String BI, posBI;
+	private String obrigatoriasBI[] = new String[20];
+	private String obrigatoriasposBI[] = new String[20];
+	private String limitadasBI[] = new String[20];
+	private String limitadasposBI[] = new String[20];
+	private String livreBI[] = new String[20];
+	private String livreposBI[] = new String[20];
+	
+	protected String curso (String bacharelado)
+	{
+		if(bacharelado == BI)
+		{
+			//desenvolver métodos
+			return BI;
+		}
+		else 
+		{
+			//desenvolver métodos
+			return posBI;
+		}
+	}
+	
+
+}
---
+++ AutoMatric/src/Classificacao.java
@@ -0,0 +1,5 @@
+
+public interface Classificacao {
+	String codigoDisciplina="";
+	String nomeDisciplina="";
+}
